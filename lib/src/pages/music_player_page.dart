b5e8a0807c697740fa7eb6fccde3eac8b5f140ce
goandreus <andreshtml5@gmail.com>
2020-04-11 18:52:53 -0500

    format document

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -5,34 +5,23 @@ class MusicPlayerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-          children: <Widget>[
-            CustomAppBar(),
-            ImageDiscoDuration()
-          ]
-        ),
+      body: Column(children: <Widget>[CustomAppBar(), ImageDiscoDuration()]),
     );
   }
 }
 
 class ImageDiscoDuration extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Row(
-        children: <Widget>[
-          ImageDisco()
-        ],
+        children: <Widget>[ImageDisco()],
       ),
     );
   }
 }
 
 class ImageDisco extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -41,39 +30,32 @@ class ImageDisco extends StatelessWidget {
       height: 250,
       child: ClipRRect(
         borderRadius: BorderRadius.circular(200),
-        child: Stack(
-          alignment: Alignment.center,
-            children:<Widget>[
-                Image(image: AssetImage('assets/aurora.jpg'),),
-                Container(
-                  width: 25,
-                  height: 25,
-                  decoration: BoxDecoration(
-                    color: Colors.black38,
-                    borderRadius: BorderRadius.circular(100)
-                  ),
-                ),
-                Container(
-                  width: 18,
-                  height: 18,
-                  decoration: BoxDecoration(
-                    color: Color(0xff1C1C25),
-                    borderRadius: BorderRadius.circular(100)
-                  ),
-                ),
-            ] 
+        child: Stack(alignment: Alignment.center, children: <Widget>[
+          Image(
+            image: AssetImage('assets/aurora.jpg'),
           ),
+          Container(
+            width: 25,
+            height: 25,
+            decoration: BoxDecoration(
+                color: Colors.black38,
+                borderRadius: BorderRadius.circular(100)),
+          ),
+          Container(
+            width: 18,
+            height: 18,
+            decoration: BoxDecoration(
+                color: Color(0xff1C1C25),
+                borderRadius: BorderRadius.circular(100)),
+          ),
+        ]),
       ),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(200),
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          colors: [
+          borderRadius: BorderRadius.circular(200),
+          gradient: LinearGradient(begin: Alignment.topLeft, colors: [
             Color(0xff484750),
-            Color(0xff1E1C24), 
-          ]
-        )
-      ),
+            Color(0xff1E1C24),
+          ])),
     );
   }
 }
