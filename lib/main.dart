d5ee90e1a20fcc277b930f76a5d6eadd49772086
goandreus <andreshtml5@gmail.com>
2020-04-11 17:37:58 -0500

    helpers inital project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:music_ui/src/pages/music_player_page.dart';
+import 'package:music_ui/src/theme/theme.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        title: 'Music',
+        theme: myTheme,
+        home: MusicPlayerPage()
     );
   }
 }
---
+++ lib/src/theme/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final myTheme = ThemeData.dark().copyWith(
+  scaffoldBackgroundColor: Color(0xff201E28),
+  iconTheme: IconThemeData(
+    color: Colors.white.withOpacity(0.3)
+  )
+);
