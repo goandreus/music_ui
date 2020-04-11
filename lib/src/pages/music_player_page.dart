a0f242a9baa87646e8f90c5e074f15b852364b1b
goandreus <andreshtml5@gmail.com>
2020-04-11 17:53:16 -0500

    custombar

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:music_ui/src/widgets/custom_appbar.dart';
 
 class MusicPlayerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        
+    return Scaffold(
+        body: Column(
+          children: <Widget>[
+            CustomAppBar()
+          ]
+        ),
     );
   }
 }
